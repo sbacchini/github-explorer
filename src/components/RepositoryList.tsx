3ddfba3b0eab8d4a511181c16fcf1f2743f73970
Stefano Bacchini <stebacchini@gmail.com>
2021-12-24 13:01:28 -0300

    Add typescript and adapt elements

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,14 +1,18 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
-import { useState, useEffect } from "react/cjs/react.development";
+import { useState, useEffect } from 'react';
 
 //https://api.github.com/orgs/rocketseat/repos
 
-
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
 
 export function RepositoryList () {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ const isDevelopement = process.env.NODE_ENV !== 'production';
 module.exports = {
     mode: isDevelopement ? 'development' : 'production',
     devtool: isDevelopement ? 'eval-source-map' : 'source-map',	
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     devServer: {
         static: path.resolve(__dirname, 'public'),
@@ -28,7 +28,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.[jt]sx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
